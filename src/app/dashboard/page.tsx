e9f655014b60c2723d916070d3b537d787ba2f0d
jespermhl <kinsej@posteo.de>
2026-03-05 00:10:30 +0100

    feat: set dashboard page to force-dynamic.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import { api, HydrateClient } from "~/trpc/server";
 import { DashboardContent } from "../_components/dashboard-page";
 
+export const dynamic = "force-dynamic";
+
 export default async function DashboardPage() {
   void api.block.getAll.prefetch();
   return (
